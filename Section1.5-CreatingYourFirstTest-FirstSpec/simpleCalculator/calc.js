9d58f2b377b5a209ba422533a9ef62f582dd7f70
javales <46269614+javales@users.noreply.github.com>
2020-07-18 23:53:15 -0700

    Lesson 10 and 11 done

---
+++ Section1.5-CreatingYourFirstTest-FirstSpec/simpleCalculator/calc.js
@@ -0,0 +1,27 @@
+function Calculator () {
+    this.total = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    return this.total = this.total += number;
+
+}
+
+Calculator.prototype.subtract = function(number) {
+    return this.total = this.total -= number;
+
+}
+
+Calculator.prototype.multiply = function(number) {
+    return this.total = this.total *= number;
+
+}
+
+Calculator.prototype.divide = function(number) {
+    if (number === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    
+    return this.total = this.total /= number;
+
+}
