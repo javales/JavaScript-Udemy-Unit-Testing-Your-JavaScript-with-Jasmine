99d60452748502ba421ed0d9284d2c3f66bf47dd
javales <46269614+javales@users.noreply.github.com>
2020-07-20 01:19:16 -0700

    Lesson 16 Spec done

--- Section2-JasmineFunctions/simpleCalculator/calc.spec.js
+++ Section2-JasmineFunctions/simpleCalculator/calc.spec.js
@@ -1,3 +1,21 @@
 describe('calc.js', function() {
+    it('should add numbers to total', function() {
+        // TODO: Expectations
 
+    });
+
+    it('should subract numbers to total', function() {
+        // TODO: Expectations
+
+    });
+
+    it('should multiply numbers to total', function() {
+        // TODO: Expectations
+
+    });
+
+    it('should divide numbers to total', function() {
+        // TODO: Expectations
+
+    });
 });
