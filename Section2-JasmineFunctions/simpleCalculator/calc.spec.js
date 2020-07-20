9936646399feb1c74a881279f63f6d38d21e3493
javales <46269614+javales@users.noreply.github.com>
2020-07-20 14:37:41 -0700

    Lesson 17 done

--- Section2-JasmineFunctions/simpleCalculator/calc.spec.js
+++ Section2-JasmineFunctions/simpleCalculator/calc.spec.js
@@ -1,21 +1,33 @@
 describe('calc.js', function() {
     it('should add numbers to total', function() {
-        // TODO: Expectations
+        const calculator = new Calculator();
+        calculator.add(5);
+        expect(calculator.total).toBe(5);
+        //expect(10).toBe(10);
 
     });
 
-    it('should subract numbers to total', function() {
-        // TODO: Expectations
+    it('should subract numbers from total', function() {
+        const calculator = new Calculator();
+        calculator.total = 30;
+        calculator.subtract(5);
 
+        expect(calculator.total).toBe(25);
     });
 
-    it('should multiply numbers to total', function() {
-        // TODO: Expectations
-
+    it('should multiply numbers by total', function() {
+        const calculator = new Calculator();
+        calculator.total = 100;
+        calculator.multiply(2);
+    
+        expect(calculator.total).toBe(200);
     });
 
-    it('should divide numbers to total', function() {
-        // TODO: Expectations
-
+    it('should divide numbers into total', function() {
+        const calculator = new Calculator();
+        calculator.total = 200;
+        calculator.divide(2);
+    
+        expect(calculator.total).toBe(100);
     });
 });
